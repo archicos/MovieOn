5b0cc7f48e7d2f9993c8b2e9fee57dba6ecfb27b
ifs21011-itdel <archicosemb@gmail.com>
2024-04-18 09:17:33 +0700

    feat configure database, models, and handler

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Frontend implemented using React.js. To run the frontend service, use command:
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+### Backend
+
+Backend implemented using Golang. To run the backend service, use command:
+
+`go run cmd/api/main.go`
+
+Open [http://localhost:4000](http://localhost:4000) to view it in your browser.
--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"server/models"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -13,12 +19,21 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type AppStatus struct {
-	Status      string `json:"status`
+	Status      string `json:"status"`
 	Environment string `json:"environment"`
 	Version     string `json:"version"`
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+	models models.Models
 }
 
 func main() {
@@ -26,29 +41,54 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on ")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres@localhost/id_goreactmovie?sslmode=disable", "Postgres connection config")
 	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+		models: models.NewModels(db),
+	}
 
 	fmt.Println("Server is running...")
 
-	http.HandleFunc("/status", func(rw http.ResponseWriter, r *http.Request) {
-		currentStatus := AppStatus{
-			Status:      "Online",
-			Environment: cfg.env,
-			Version:     version,
-		}
+	serve := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
 
-		res, err := json.MarshalIndent(currentStatus, "", "\t")
-		if err != nil {
-			fmt.Println(err)
-		}
+	logger.Printf("Starting sever on port ", cfg.port)
 
-		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		rw.Write(res)
-	})
-
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	err = serve.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
